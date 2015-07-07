0a57ca5234fd0cfe86094b1e541e541e75f6651f
juancjara <juanc.jara@pucp.pe>
2015-07-07 18:07:55 -0400

    added some chrome api functions, localforage and default icon for notification

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log("here");
+var Notification = require('./chrome-api/notification');
+var Alarm = require('./chrome-api/alarm');
+var Storage = require('./helpers/storage');
+
+Notification.create({message: 'msg'});
+
+var doSomething = function() {
+  console.log('doing something');
+};
+
+Alarm.create('create notification',
+             {when: Date.now() + 1000},
+              doSomething);
+
+Storage.set({foo: 'bar'}, function(data) {
+  console.log('value saved');
+  Storage.get('foo', function(value) {
+    console.log('value found', value)
+  })
+})
+
+//promises
